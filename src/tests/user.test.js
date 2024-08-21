91ce51850d27efe436b988380daa6b4d7337ce27
Vicent-Duarte <vicentduarte70@gmail.com>
2024-08-20 23:21:11 -0400

    creacion de test

---
+++ src/tests/user.test.js
@@ -0,0 +1,43 @@
+const request  = require("supertest");
+const app = require("../app");
+
+const BASE_URL = '/api/v1/users';
+
+beforeAll(async()=>{
+    const user = {
+        email: "johndoe@gmail.com",
+        password: "secret123"
+    }
+    const res = await request(app)
+        .post(`${BASE_URL}/login`)
+        .send(user)
+
+    TOKEN = res.body.token
+
+    console.log(TOKEN)
+})
+
+const user = {
+    firstName: "Vicent",
+    lastName: "Duarte",
+    email: "vicentduarte@gmail.com",
+    password: "12640330",
+    phone: "+18293587070"
+}
+
+test("POST -> BASE_URL , should return statusCode 201, and res.body. firstName === user.firstName", async () => {
+    // const columns = ["firstName, lastName, email, password, phone"]
+    const res = await request(app)
+
+        .post(BASE_URL)
+        .send(user)
+
+    expect(res.statusCode).toBe(201);
+    expect(res.body).toBeDefined();
+
+    // columns.forEach((columns) => {
+    //     expect(res.body).toHaveProperty(columns);
+    // })
+
+    expect(res.body.firstName).toBe(user.firstName);
+})
---
+++ src/tests/userCreate.js
@@ -0,0 +1,16 @@
+const User = require("../models/User")
+
+const userCreate = async () => {
+
+    const user = {
+        firstName: "John",
+        lastName: "Doe",
+        email: "johndoe@gmail.com",
+        password: "secret123",
+        phone: "+18293580707"
+    }
+
+    await User.create(user)
+}
+
+module.exports =  userCreate;
