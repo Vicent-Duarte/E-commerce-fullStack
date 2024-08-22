a6a77b80f73ebde3fc0fe60dd4e9d53096082f8f
Vicent-Duarte <vicentduarte70@gmail.com>
2024-08-22 02:41:30 -0400

    test fallidos

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -36,22 +36,19 @@ const update = catchError(async (req, res) => {
   return res.json(result[1][0]);
 });
 
-const login = catchError(async (req, res) => {
+const login = catchError(async(req, res) => {
   const { email, password } = req.body
-  const user = await User.findOne({ where: { email } })
-  if (!user) return res.status(401).json({ message: "User not foud" })
-
-  const isValid = await bcrypt.compare(password, user.password)
-  if (!isValid) return res.sendStatus(401)
-
+  const user = await User.findOne({ where: {email} })
+  if(!user) return res.status(401).json({"message": "Las credenciales ingresadas son incorrectas"})
+      const isValid = await bcrypt.compare(password, user.password)
+  if(!isValid) return res.status(401).json({"message": "Las credenciales ingresadas son incorrectas"})
   const token = jwt.sign(
-    { user },
-    process.env.TOKEN_SECRET,
-    { expiresIn: '1d' }
+      {user},
+      process.env.TOKEN_SECRET,
+      {expiresIn : '1d'}
   )
-
-  return res.json({ user, token })
-})
+  return res.status(200).json({user, token});
+});
 
 module.exports = {
   getAll,
--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -1,57 +1,107 @@
-const request  = require("supertest");
-const app = require("../app");
+const request = require("supertest")
+const app = require('../app')
+const supertest = require("supertest")
 
 const BASE_URL = '/api/v1/users'
 let TOKEN
+let userId
 
 beforeAll(async () => {
   const user = {
-    email: "juan@gmail.com",
-    password: "juan1234"
+  email: "vicentduarte@gmail.com",
+  password: "12640330"
   }
   const res = await request(app)
     .post(`${BASE_URL}/login`)
     .send(user)
   TOKEN = res.body.token
-  console.log(TOKEN);
+  // console.log(TOKEN);
 })
 
 const user = {
-    firstName: "Vicent",
-    lastName: "Duarte",
-    email: "vicentduarte@gmail.com",
-    password: "12640330",
-    phone: "+18293587070"
+  firstName: "Vicent",
+  lastName: "Duarte",
+  email: "vicentduarte@gmail.com",
+  password: "12640330",
+  phone: "+182935820707"
 }
 
-test("POST -> BASE_URL , should return statusCode 201, and res.body. firstName === user.firstName", async () => {
-    // const columns = ["firstName, lastName, email, password, phone"]
-    const res = await request(app)
+test("POST -> BASE_URL, should return statusCode 201, and res.body.firstName === user.firstName", async () => {
 
-        .post(BASE_URL)
-        .send(user)
+  const columns = ['firstName', 'lastName', 'email', 'password', 'phone']
+  const res = await request(app)
+    .post(BASE_URL)
+    .send(user)
 
-    expect(res.statusCode).toBe(201);
-    expect(res.body).toBeDefined();
+  userId = res.body.id
 
-    // columns.forEach((columns) => {
-    //     expect(res.body).toHaveProperty(columns);
-    // })
-
-    expect(res.body.firstName).toBe(user.firstName);
+  expect(res.statusCode).toBe(201)
+  expect(res.body).toBeDefined()
+  // columns.forEach((column) => {
+  // })
+  expect(res.body.firstName).toBeDefined()
+  expect(res.body.firstName).toBe(user.firstName)
 })
 
-test("POST -> BASE_URL/LOGIN , should return statusCode 200, and res.body.user.email === hits.email", async () => {
-    const hits = {
-        email: "vicentduarte@gmail.com",
-        password: "12640330"
-    }
-    const res = await request(app)
-     .post(`${BASE_URL}/login`)
-     .send(hits)
+test("GET -> BASE_URL, should return statusCode 200, and res.body.length === 2", async () => {
 
-     expect(res.statusCode).toBe(200);
-     expect(res.body).toBeDefined();
-     expect(res.body.token).toBeDefined();
-     expect(res.body.hits.email).toBe(hits.email);
+  const res = await supertest(app)
+    .get(BASE_URL)
+    .set('Authorization', `Bearer ${TOKEN}`)
+
+  expect(res.statusCode).toBe(200)
+  expect(res.body).toBeDefined()
+  expect(res.body).toHaveLength(2)
+})
+
+test("PUT -> 'BASE_URL/:ID', should return statusCode 200, and res.body.firstName === userUpdate.firstName", async () => {
+
+  const userUpdate = {
+    firstName: "Vicent"
+  }
+  const res = await request(app)
+    .put(`${BASE_URL}/${userId}`)
+    .send(userUpdate)
+    .set('Authorization', `Bearer ${TOKEN}`)
+
+  expect(res.statusCode).toBe(200)
+  expect(res.body).toBeDefined()
+  expect(res.body.firstName).toBe(userUpdate.firstName)
+})
+
+test("POST -> 'BASE_URL/LOGIN', should return status code 200, and res.body.user.email === hits.email", async () => {
+  const hits = {
+    email: "vicentduarte@gmail.com",
+    password: "12640330"
+  }
+
+  const res = await request(app)
+    .post(`${BASE_URL}/login`)
+    .send(hits)
+
+  expect(res.statusCode).toBe(200)
+  expect(res.body).toBeDefined()
+  expect(res.body.user).toBeDefined()
+  expect(res.body.token).toBeDefined()
+  expect(res.body.user.email).toBe(hits.email)
+
+})
+
+
+test("POST -> 'BASE_URL/LOGIN', should return status code 401", async () => {
+  const hits = {
+    email: "vicentduarte@gmail.com",
+    password: "12640330"
+  }
+  const res = await request(app)
+    .post(`${BASE_URL}/login`)
+    .send(hits)
+  expect(res.statusCode).toBe(401)
+})
+
+test("DELETE -> 'BASE_URL/:ID', should return statusCode 204", async () => {
+  const res = await request(app)
+    .delete(`${BASE_URL}/${userId}`)
+    .set('Authorization', `Bearer ${TOKEN}`)
+  expect(res.statusCode).toBe(204)
 })
