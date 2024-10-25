3ada14b1117e92a22c2f0ec6276b42724b9756bc
Vicent-Duarte <vicentduarte70@gmail.com>
2024-10-25 00:16:12 -0400

    verificacion

--- src/utils/verifyJWT.js
+++ src/utils/verifyJWT.js
@@ -3,17 +3,21 @@ require('dotenv').config();
 
 const verifyJwt = (req, res, next) => {
   const authHeader = req.headers.authorization || req.headers.Authorization;
-  if (!authHeader?.startsWith('Bearer ')) return res.sendStatus(401);
+  
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Token no proporcionado o formato incorrecto' });
+  }
+
   const token = authHeader.split(' ')[1];
-  jwt.verify(
-    token,
-    process.env.TOKEN_SECRET,
-    (err, decoded) => {
-      if (err) return res.sendStatus(403);
-      req.user = decoded.user;
-      next();
+  
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: 'Token no válido' });
     }
-  )
+    
+    req.user = decoded.user;
+    next();
+  });
 }
 
 module.exports = { verifyJwt };
